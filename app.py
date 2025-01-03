e17a2f20fca2003119602bbafb5862bc630bf1c3
yashgondaliya36 <yashgondaliya36@gmail.com>
2025-01-03 18:37:57 +0530

    update upload files part

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import streamlit as st
 import os
 from PIL import Image
-
+import streamlit as st
+import pandas as pd
+from IA_Logger import logger
 # Set up a 'local database' folder
 if not os.path.exists('local_database'):
     os.makedirs('local_database')
@@ -17,14 +18,19 @@ uploaded_files = st.file_uploader("Choose JPG files", type=["jpg"], accept_multi
 saved_file_paths = []
 
 if uploaded_files:
-    for uploaded_file in uploaded_files:
-        # Read the image
-        img = Image.open(uploaded_file)
-        
-        # Save the image in the 'local database' folder
-        file_path = os.path.join('local_database', uploaded_file.name)
-        img.save(file_path)
-        saved_file_paths.append(file_path)
+    try:
+        for uploaded_file in uploaded_files:
+            # Read the image
+            img = Image.open(uploaded_file)
+            
+            # Save the image in the 'local database' folder
+            file_path = os.path.join('local_database', uploaded_file.name)
+            img.save(file_path)
+            saved_file_paths.append(file_path)
 
-    # Show a success message
-    st.success(f"{len(uploaded_files)} file(s) uploaded and saved successfully.")
+        # Show a success message
+        st.success(f"{len(uploaded_files)} file(s) uploaded and saved successfully.")
+    
+    except Exception as e:
+        st.error(f"Error during file upload: {e}")
+        logger.error(f"Error during file upload: {e}")
